188cbb68440821e4afd603527628cc8cb8fa87db
igor rocha <igortice@gmail.com>
2018-03-13 13:52:10 -0300

    better

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls:   [ './home.component.scss' ]
 })
 export class HomeComponent implements OnInit {
+  cards = [
+    {
+      id: 1, order: 1, name: 'Card Name 1', tasks: [
+        { id: 1, order: 1, checked: false, desc: 'test check task 1' },
+        { id: 2, order: 2, checked: true, desc: 'test check task 2' },
+      ]
+    },
+    {
+      id: 2, order: 2, name: 'Card Name 2', tasks: [
+        { id: 1, order: 1, checked: true, desc: 'test check task 3' },
+        { id: 2, order: 2, checked: false, desc: 'test check task 4' },
+      ]
+    }
+  ];
 
   constructor() { }
 
