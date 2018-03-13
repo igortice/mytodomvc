849d0960268ece5e9a017263be5b8339aef6bf8b
Igor Rocha <igortice@gmail.com>
2018-03-12 22:59:56 -0300

    init layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { MainComponent } from './layout/main/main.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports:      [
     BrowserModule
