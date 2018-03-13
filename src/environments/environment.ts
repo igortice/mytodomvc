1ce6447ce5f2aa846a6d7fdf6adcb8da5d47494d
Igor Rocha <igortice@gmail.com>
2018-03-12 22:51:30 -0300

    better

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,8 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  system:     {
+    name: 'My Todo MVC Angular 5'
+  }
 };
