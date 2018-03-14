cfd1d78c5b05b0eb4d461308735181f4ec5b37aa
igor rocha <igortice@gmail.com>
2018-03-14 11:42:05 -0300

    better

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,29 +10,22 @@ export class HomeComponent implements OnInit {
   cards = [
     {
       id: 1, order: 1, name: 'Card Name 1', tasks: [
-        { id: 1, order: 1, checked: false, desc: 'test check task 1' },
-        { id: 2, order: 2, checked: true, desc: 'test check task 2' }
+        { id: 1, order: 1, checked: false, desc: 'card 1 task 1' },
+        { id: 2, order: 2, checked: true, desc: 'card 1 task 2' }
       ]
     },
     {
       id: 2, order: 2, name: 'Card Name 2', tasks: [
-        { id: 1, order: 1, checked: true, desc: 'test check task 3' },
-        { id: 2, order: 2, checked: false, desc: 'test check task 4' }
+        { id: 1, order: 1, checked: true, desc: 'card 2 task 1' },
+        { id: 2, order: 2, checked: false, desc: 'card 2 task 2' }
       ]
     }
   ];
 
-  optionCards: SortablejsOptions = {
-    onUpdate: (event: any) => {
-      console.log(event);
-    }
-  };
+  optionCards: SortablejsOptions = {};
 
   optionTasks: SortablejsOptions = {
-    group:    'tasks',
-    onUpdate: (event: any) => {
-      console.log(event);
-    }
+    group: 'tasks'
   };
 
   constructor() {
